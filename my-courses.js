fe26f23941ca7532d8038f1cff41122240003b2e
Lana Shyshova <lanashyshova@gmail.com>
2023-03-09 14:12:36 -0800

    Add test data

--- my-courses.js
+++ my-courses.js
@@ -1,3 +1,43 @@
+// Copy over models while figuring out the imports
+function Course(id, hasEnded) {
+    this.id = id;
+    this.hasEnded = hasEnded;
+}
+
+function Enrollment(isEnrolled) {
+    this.isEnrolled = isEnrolled;
+
+    this.isUnenrolled = function() {
+        return !this.isEnrolled;
+    }
+}
+
+function Membership(userId, courseId, enrollmentPromise) {
+    this.userId = userId;
+    this.courseId = courseId;
+    this.enrollmentPromise = enrollmentPromise;
+}
+
+// Create courses instances
+const course1 = new Course(1, false);
+const course2 = new Course(2, true);
+const course3 = new Course(3, false);
+
+// Create enrollments instances
+const enrollment1 = new Enrollment(true);
+const enrollment2 = new Enrollment(false);
+
+// Create test memberships
+const membership1 = new Membership(1, 1, Promise.resolve(enrollment1));
+const membership2 = new Membership(1, 2, Promise.resolve(enrollment2));
+const membership3 = new Membership(2, 1, Promise.resolve(enrollment1));
+const membership4 = new Membership(2, 2, Promise.resolve(enrollment2));
+const membership5 = new Membership(3, 3, Promise.resolve(enrollment1));
+const membership6 = new Membership(3, 3, Promise.resolve(enrollment2));
+
+const courses = [course1, course2, course3];
+const memberships = [membership1, membership2, membership3, membership4, membership5, membership6];
+
 var MyCourses = {
     courses: function (cs, ms) {
         // There shouldn't be any memberships with missing courses, but just in
@@ -23,13 +63,14 @@ var MyCourses = {
                 });
         })).then(function (coursesRoles) {
             // Filter the courses to current courses
-            currentCourses = courseRoles.filter(function (cr) {
+            currentCourses = coursesRoles.filter(function (cr) {
                 return !cr.course.hasEnded;
             });
             // Filter for courses where the user's membership is unenrolled
-            unenrolledCourses = courseRoles.filter(function (cr) {
+            unenrolledCourses = coursesRoles.filter(function (cr) {
                 return cr.enrollment.isUnenrolled();
             });
+
             // Merge the two
             coursesRoles = currentCourses.concat(unenrolledCourses);
 
@@ -54,3 +95,10 @@ var MyCourses = {
         });
     }
 };
+
+// Call the courses method with the sample inputs
+MyCourses.courses(courses, memberships).then(function (result) {
+  console.log(result);
+}).catch(function (error) {
+  console.error(error);
+});
