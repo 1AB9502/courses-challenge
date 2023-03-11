573202a348448d3fc72ff625b3d7c4ecdb679f84
Lana Shyshova <lanashyshova@gmail.com>
2023-03-11 14:03:28 -0800

    Rewrite getCourses() from scratch

--- my-courses.js
+++ my-courses.js
@@ -1,59 +1,37 @@
-const { courses, memberships } = require('./data');
+const { courses, memberships } = require("./data");
 
 class MyCourses {
-  constructor(courses, memberships) {
-      this.courses = courses;
-      this.memberships = memberships;
-  }
+    constructor(courses, memberships) {
+        this.courses = courses;
+        this.memberships = memberships;
+    }
 
-  getCourses() {
-      // Filter out memberships with missing courses
-      const memberships = this.memberships.filter(({ courseId }) =>
-          this.courses.find(({ id }) => courseId === id)
-      );
-
-      // Get courses belonging to a membership, and their enrollments in those courses
-      return Promise.all(
-          memberships.map((membership) =>
-              membership.enrollmentPromise.then((enrollment) => ({
-                  course: this.courses.find(
-                      ({ id }) => id === membership.courseId
-                  ),
-                  enrollment,
-              }))
-          )
-      )
-          .then((coursesEnrollments) => {
-              // Filter out courses that have ended and combine with courses where user's membership is unenrolled
-              const filteredCourses = coursesEnrollments
-                  .filter((coursesEnrollment) => !coursesEnrollment.course.hasEnded)
-                  .concat(
-                      coursesEnrollments.filter((coursesEnrollment) =>
-                          coursesEnrollment.enrollment.isUnenrolled()
-                      )
-              );
-              // Sort the courses so that unenrolled courses are at the top
-              const sortedCourses = filteredCourses.sort((courseA, courseB) =>
-                  courseA.enrollment.isUnenrolled() &&
-                  courseB.enrollment.isEnrolled
-                      ? -1
-                      : courseA.enrollment.isEnrolled &&
-                        courseB.enrollment.isUnenrolled()
-                      ? 1
-                      : 0
-              );
-
-              return sortedCourses.map(({ course }) => course);
-          })
-          .catch((error) => {
-              console.error(
-                  "An error occurred while fetching membership data:",
-                  error
-              );
-              throw error;
-          });
-  }
-}
+    async getCourses() {
+        const enrolledCourses = [];
+    
+        try {
+            for (const membership of this.memberships) {
+                const enrollment = await membership.enrollmentPromise;
+                // If the user is enrolled in the course and the membership is not missing any courses,
+                // find corresponding course in the "courses" array.
+                if (enrollment.isEnrolled && membership.courseId !== null) {
+                    const course = this.courses.find(
+                        (c) => c.id === membership.courseId
+                    );
+                    // If the course exists and has not ended,
+                    // add it to the "enrolledCourses" array.
+                    if (course && !course.hasEnded) {
+                        enrolledCourses.push(course);
+                    }
+                }
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    
+        return enrolledCourses;
+    }
+}    
 
 // Call the getCourses method with the sample inputs
 const myCourses = new MyCourses(courses, memberships);
